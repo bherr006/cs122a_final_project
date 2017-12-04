1bc3947ed2a6697f26a9388bd4463a3bb78c5439
bherr006 <bherr006@ucr.edu>
2017-12-04 10:18:15 -0800

    Update 122a_final_project_final_edit.cpp

--- 122a_final_project_final_edit.cpp
+++ 122a_final_project_final_edit.cpp
@@ -4,6 +4,18 @@
  * Created: 11/13/2017 9:30:46 AM
  * Author : brandon
  */ 
+/* Nokia 5110 LCD AVR Library example
+ *
+ * Copyright (C) 2015 Sergey Denisov.
+ * Written by Sergey Denisov aka LittleBuster (DenisovS21@gmail.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence
+ * as published by the Free Software Foundation; either version 3
+ * of the Licence, or (at your option) any later version.
+ *
+ * Original library written by SkewPL, http://skew.tk
+ */
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
